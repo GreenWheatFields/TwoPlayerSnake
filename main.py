2ccd8acf1d10094587a39cd9fba2c42b0907491e
kamalh2001 <63170492+kamalh2001@users.noreply.github.com>
2020-09-28 01:10:37 -0400

    can detect collision when snake is two blocks

--- main.py
+++ main.py
@@ -23,7 +23,7 @@ class Food:
         print(self.food)
         if self.food[1] == 300:
             self.food[1] = 160
-        #todo, y value of 300 doesnt show
+        # todo, y value of 300 doesnt show
 
     def draw(self):
         pygame.draw.rect(board.dis, (24, 252, 0), [self.food[0], self.food[1], 10, 10])
@@ -33,26 +33,28 @@ class Snake():
     def __init__(self, x: int, y: int, board: Board):
         self.board = board
         pygame.draw.rect(board.dis, white, [x, y, 10, 10])
-        self.snake = [[x,y]]
+        self.snake = [[x, y]]
 
     def draw(self, newX, newY):
         # # draw a snake
-        self.snake.append([newX,newY])
+        self.snake.append([newX, newY])
         self.snake.pop(0)
         for i in self.snake:
             pygame.draw.rect(self.board.dis, white, [i[0], i[1], 10, 10])
 
-
     def eat(self, x, y):
         self.snake.append([x, y])
 
-    def isCollision(self,x,y):
-        if [x,y] in self.snake[0:len(self.snake) -1]:
-            # doesnt work when size is 2
-            print("herehere")
-            print(self.snake)
-            print([x,y])
+    def isCollision(self, x, y):
+        if [x, y] in self.snake[0:len(self.snake) - 1]:
             return True
+        elif len(self.snake) == 2:
+            if y == 0:
+                if self.snake[1][0] + x == self.snake[0][0]:
+                    return True
+            elif x == 0:
+                if self.snake[1][1] + y == self.snake[0][1]:
+                    return True
 
 
 class Game:
@@ -87,14 +89,14 @@ class Game:
                         x_change = 0
                         y_change = 10
             if xPosistion > 400 or xPosistion < 0 or yPosistion > 300 or yPosistion < 0:
-                print(xPosistion, yPosistion)
-                game_over = True
-            elif xPosistion == food.food[0] and yPosistion == food.food[1]:
-                # game_over = True
-                snake.eat(food.food[0],food.food[1])
-                food = Food()
-            elif snake.isCollision(xPosistion, yPosistion):
                 Game.game_over()
+            elif xPosistion == food.food[0] and yPosistion == food.food[1]:
+                snake.eat(food.food[0], food.food[1])
+                food = Food()
+            elif snake.isCollision(x_change, y_change):
+                Game.game_over()
+
+
 
             xPosistion += x_change
             yPosistion += y_change
@@ -103,10 +105,13 @@ class Game:
             food.draw()
             snake.draw(xPosistion, yPosistion)
             pygame.display.update()
-            clock.tick(15)
-        game_over()
+            clock.tick(5)
+        pygame.quit()
+        quit()
+
     @staticmethod
     def game_over():
+        # just so display doesnt update after game over
         pygame.quit()
         quit()
 
