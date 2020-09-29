5d77a2917ee551fdf9b1596bd64a1c3ac1fe600c
kamalh2001 <63170492+kamalh2001@users.noreply.github.com>
2020-09-29 00:34:47 -0400

    snakes can no longer spawn inside a snake

--- Snake.py
+++ Snake.py
@@ -4,7 +4,7 @@ import pygame
 import random
 
 white = (255, 255, 255)
-width, height = 400, 300
+width, height = 500, 500
 global board
 
 
@@ -15,22 +15,17 @@ class Board():
 
 class Food:
     def __init__(self, snake, squares: list):
-        valid_squares = squares
-        for j in snake:
-            for i, index in enumerate(valid_squares):
-                if j == i:
-                    #todo, doesnt work
-                    print("here")
-                    valid_squares.pop(index)
-                    continue
-        self.food = [round(random.randrange(width - 10), -1), round(random.randrange(height), -1)]
-        # todo, dont generate food inside a snake. Random numbers would get very inefficent as snake grew bigger
-        # self.food = [0, 0]
-        if self.food[1] == 300:
-            self.food[1] = 160
-
+        self.food = self.spawnFood(squares, snake)
     def draw(self):
         pygame.draw.rect(board.dis, (24, 252, 0), [self.food[0], self.food[1], 10, 10])
+    
+    def spawnFood(self ,squares, snake):
+        valid_squares = list(squares)
+        for j in snake:
+            for index, i in enumerate(valid_squares):
+                if j == i:
+                    valid_squares.pop(index)
+        return random.choice(valid_squares)
 
 
 class Snake():
@@ -40,7 +35,6 @@ class Snake():
         self.snake = [[x, y]]
 
     def draw(self, newX, newY):
-        # # draw a snake
         self.snake.append([newX, newY])
         self.snake.pop(0)
         for i in self.snake:
@@ -66,10 +60,11 @@ class Game:
     def __init__(self):
         self.score = 0
         self.squares = []
-        for i in range(0,310, 10):
-            print(i)
-            for j in range(0,400, 10):
+        x = ([1,2], [2,1])
+        for i in range(0,width, 10):
+            for j in range(0,height, 10):
                 self.squares.append([i,j])
+        self.squares = tuple(self.squares)
 
     def start(self):
         pygame.init()  # init outside class?
@@ -102,9 +97,9 @@ class Game:
                     elif event.key == pygame.K_DOWN:
                         x_change = 0
                         y_change = 10
-            if xPosistion > 400 or xPosistion <= 0 or yPosistion > 300 or yPosistion <= 0:
-                #todo, right side border too big
-                self.game_over()
+            print(xPosistion, yPosistion)
+            if xPosistion > width - 10 or xPosistion < 0 or yPosistion >= height or yPosistion < 0:
+               self.game_over()
             elif xPosistion == food.food[0] and yPosistion == food.food[1]:
                 snake.eat(food.food[0], food.food[1])
                 self.score += 1
@@ -128,7 +123,6 @@ class Game:
         quit()
 
     def game_over(self):
-        # just so display doesnt update after game over
         pygame.quit()
         quit()
 
