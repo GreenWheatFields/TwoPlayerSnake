6cf848af2843227a49fdd86d2e3dbf059d97fa54
kamalh2001 <63170492+kamalh2001@users.noreply.github.com>
2020-10-01 15:22:18 -0400

    added more logic to the intializing of a game

--- client.py
+++ client.py
@@ -1,10 +1,13 @@
 import socket
 import time
-
+import uuid
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.connect(('localhost', 8089))
-while True:
-    s.send(str(time.time()).encode())
-    incoming = s.recv(64)
-    if len(incoming) > 0:
-        print(incoming.decode())
+usename = str(uuid.uuid4())
+s.send(usename.encode())
+# while True:
+#     print("here")
+#     s.send(str(time.time()).encode())
+#     # incoming = s.recv(64)
+#     # if len(incoming) > 0:
+#     #     print(incoming.decode())
--- server.py
+++ server.py
@@ -1,19 +1,15 @@
 import socket
 import time
-import Snake
-
-
-
-
+str
 class Server:
     def __init__(self):
         self.server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.server_socket.bind(('localhost', 8089))
-        self.server_socket.listen() 
+        self.server_socket.listen(2)
         self.conn, self.address = self.server_socket.accept()
         self.game_over = False
         self.initialized = False
-        self.addresses = []
+        self.players = {}
         self.initializeGame()
     
     def hostGame(self):
@@ -27,10 +23,21 @@ class Server:
     def initializeGame(self):
         # establish two connections
         # tell clients how to build the window
-        while len(self.addresses) < 2:
+        while len(self.players) < 2:
             incoming = self.conn.recv(64)
-            print(self.address)
-        print("here")
+            if len(incoming) > 0:
+                if incoming.decode() not in self.players:
+                    self.players[incoming.decode()] = self.address[0]
+                    self.conn.sendall() #for now, sendall, later sendto
+                else:
+                    #client querying server before intialized
+                    pass
+
+        # game width, and height
+        # client should add a username on its first handshake
+        # estabilsh game logic like who goes first etc
+if __name__ == '__main__':
+    server = Server()
             
     
 
