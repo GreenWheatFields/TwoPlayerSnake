3d8df6c09a9078546b3fcb90b222a3bc53c0ae33
kamalh2001 <63170492+kamalh2001@users.noreply.github.com>
2021-01-02 23:13:34 -0500

    added locks. moving server methods to lobby class

--- client_handler.py
+++ client_handler.py
@@ -1,5 +1,6 @@
 import socket
 import time
+import random
 
 from lobby import Lobby
 from threading import Thread
@@ -15,6 +16,7 @@ class ClientHandler(Thread):
         self.lobby = lobby
         self.width = 500
         self.height = 500
+        self.username = ""
 
     def establish_conn(self):
         pass
@@ -24,25 +26,63 @@ class ClientHandler(Thread):
                     "WIDTH": self.width,
                     "HEIGHT": self.height,
                     "WAITING": True}
-
         incoming = wait_for_message(self.conn)
         temp = incoming["USERNAME"] not in self.lobby.players
         if temp:
+            self.lobby.acquire()
             self.lobby.players.append(incoming["USERNAME"])
+            self.lobby.handlers[incoming["USERNAME"]] = self
             response["WAITING"] = False if len(self.lobby.players) >= 2 else True
+            self.lobby.release()
             response["TIME"] = time.time()
-
-            self.conn.sendto(send_json(response), self.address)
-            if not self.twoPlayers:
-                # break
-                pass
-            else:
-                pass
-
+            self.conn.send(send_json(response))
         else:
             response["TIME"] = time.time()
-            # this function can be cleaner.
             self.conn.sendall(send_json(response))
 
+    def send_build_instruction(self):
+        self.lobby.acquire()
+        if self.lobby.init_flag:
+            self.lobby.release()
+        else:
+            self.lobby.init_game_state(self.width, self.height)
+        response = {"INSTRUCTION": "BUILD",
+                    "FIRST": self.lobby.turn,
+                    "WIDTH": self.width,
+                    "HEIGHT": self.height,
+                    "TIME": time.time(),
+                    "SNAKE": self.lobby.snake.snake,
+                    "FOOD": self.lobby.food.food
+                    }
+        self.conn.send(send_json(response))
+    def wait_for_client_to_build(self):
+        flag = False
+        while True:
+            incoming_message = wait_for_message(self.conn)
+            self.lobby.acquire()
+            try:
+                if incoming_message["READY"]:
+                    self.lobby.notify_player_ready(incoming_message["USERNAME"])
+                    break
+            except KeyError:
+                pass
+            if len(self.lobby.players_ready) == 2:
+                flag = True
+        self.lobby.release()
+        if flag:
+            self.lobby.sync()
+    def send_sync_message(self, time):
+        message = {
+            "INSTRUCTION": "PLAY",
+            "STARTTIME": str(time)
+        }
+        self.conn.send(send_json(message))
     def run(self):
-        pass
+        self.make_first_contact()
+        while len(self.lobby.players) < 2:
+            pass
+        self.send_build_instruction()
+        self.wait_for_client_to_build()
+
+
+
--- lobby.py
+++ lobby.py
@@ -1,5 +1,49 @@
 # game state here. acsessed by two clientlisteners
-class Lobby:
+import time
+from threading import Lock
+import random
+import sys
+# todo, no need for this to be in server class. also these classes should have  server=False
+from server import Snake, Food
+
+
+class Lobby(Lock):
     def __init__(self):
+        super().__init__()
+        self.handlers = {}
         self.players = []
         self.assigned_threads = 0
+        self.turn = None
+        self.init_flag = False
+        self.squares = []
+        self.snake = None
+        self.food = None
+        self.players_ready = []
+
+    def init_game_state(self, width, height):
+        self.turn = random.choice(self.players)
+        temp = []
+        for i in range(0, width, 10):
+            for j in range(0, height, 10):
+                temp.append([i, j])
+        self.squares = tuple(temp)
+        self.snake = Snake(200, 150)
+        self.food = Food(self.snake.snake, self.squares)
+        self.init_flag = True
+
+    def notify_player_ready(self, username: str):
+        if username not in self.players_ready:
+            self.players_ready.append(username)
+    def sync(self):
+        sys
+        # start_time = time.time() + .25
+        # for client_handler in self.handlers.values():
+        #     pass
+        # while len(players_ready) < 2 or time.time() < self.start_time:
+        #     self.incoming_message = wait_for_message(self.conn)
+        #     if not self.twoPlayers:
+        #         break
+        #     elif self.incoming_message["USERNAME"] not in players_ready:
+        #         players_ready.append(self.incoming_message["USERNAME"])
+    def run_game(self):
+        pass
